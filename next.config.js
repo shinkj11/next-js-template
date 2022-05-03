75a825346784cbcac6812a6c9c2fc4166e02fa0a
kyeongjun.shin <>
2022-05-03 18:39:42 +0900

    Set config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: `${process.env.NEXT_PUBLIC_ASSET_PREFIX}`,
+  basePath: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+  },
+  images: { 
+    path: `${process.env.NEXT_PUBLIC_BASE_PATH}/_next/image`,
+  }
 }
 
 module.exports = nextConfig
